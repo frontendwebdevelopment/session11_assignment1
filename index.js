04615b509d18aa8ae508a72a35792c5fafb8b3b3
Ajay Kumar S M <ajaymandakki@gmail.com>
2017-03-01 17:34:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function($scope) {
+    $scope.name = "Ajay Kumar S M";
+    $scope.eid = "13";
+    $scope.designation = "Team Lead";
+});
